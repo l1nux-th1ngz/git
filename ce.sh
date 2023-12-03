460ace8f7157b98afd800d77431aee41d4d89ef0
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-12-03 06:56:29 -0700

    Create ce.sh

---
+++ ce.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Update system
+sudo apt-get update
+sudo apt-get upgrade
+
+# Install necessary packages
+sudo apt-get install -y curl openssh-server ca-certificates perl
+sudo apt-get install -y postfix
+
+# Add GitLab package repository
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+
+# Install GitLab CE version 16.4.3
+sudo EXTERNAL_URL="https://gitlab.example.com" apt-get install gitlab-ce=16.4.3-ce.0
+
+# Pin the version to limit auto-updates
+sudo apt-mark hold gitlab-ce
+
+# Show what packages are held back
+sudo apt-mark showhold
+
+# Create a basic .gitlab-ci.yml file
+echo "stages:" > .gitlab-ci.yml
+echo "  - build" >> .gitlab-ci.yml
+echo "  - test" >> .gitlab-ci.yml
+echo "" >> .gitlab-ci.yml
+echo "build_job:" >> .gitlab-ci.yml
+echo "  stage: build" >> .gitlab-ci.yml
+echo "  script: echo \"Building the app\"" >> .gitlab-ci.yml
+echo "" >> .gitlab-ci.yml
+echo "test_job:" >> .gitlab-ci.yml
+echo "  stage: test" >> .gitlab-ci.yml
+echo "  script: echo \"Testing the app\"" >> .gitlab-ci.yml
