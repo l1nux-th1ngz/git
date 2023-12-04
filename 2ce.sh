502bc596d24d4b84ea7cb65b80cb6f75e282a063
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-12-03 21:30:40 -0700

    Update 2ce.sh

--- 2ce.sh
+++ 2ce.sh
@@ -6,13 +6,25 @@ sudo apt-get upgrade
 
 # Install necessary packages
 sudo apt-get install -y curl openssh-server ca-certificates perl
-sudo apt-get install -y postfix
+sudo apt-get install -y postfix zenity
 
 # Add GitLab package repository
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
 
+# Get user information
+USER_INFO=$(zenity --forms --title="User Information" \
+    --text="Enter user information" \
+    --add-entry="Username" \
+    --add-password="Password" \
+    --add-entry="Domain")
+
+# Parse user information
+USERNAME=$(echo $USER_INFO | cut -d'|' -f1)
+PASSWORD=$(echo $USER_INFO | cut -d'|' -f2)
+DOMAIN=$(echo $USER_INFO | cut -d'|' -f3)
+
 # Install GitLab CE version 16.4.3
-sudo EXTERNAL_URL="https://gitlab.example.com" apt-get install gitlab-ce=16.4.3-ce.0
+sudo EXTERNAL_URL="https://$DOMAIN" apt-get install gitlab-ce=16.4.3-ce.0
 
 # Pin the version to limit auto-updates
 sudo apt-mark hold gitlab-ce
@@ -42,4 +54,11 @@ echo "" >> ~/Documents/gitlab_instructions.txt
 echo "You can access the GitLab web interface at the following URL:" >> ~/Documents/gitlab_instructions.txt
 echo "http://$IP_ADDRESS" >> ~/Documents/gitlab_instructions.txt
 echo "" >> ~/Documents/gitlab_instructions.txt
-echo "The default username is 'root'. During the first visit, you'll be redirected to a password reset screen to provide the password for the initial administrator account." >> ~/Documents/gitlab_instructions.txt
+echo "The username is '$USERNAME'. During the first visit, you'll be redirected to a password reset screen to provide the password for the initial administrator account." >> ~/Documents/gitlab_instructions.txt
+
+# Create a document with user information
+echo "Username: $USERNAME" > ~/Documents/user_info.txt
+echo "Password: $PASSWORD" >> ~/Documents/user_info.txt
+echo "Domain: $DOMAIN" >> ~/Documents/user_info.txt
+echo "" >> ~/Documents/user_info.txt
+echo "Please save this file in a different location (example: a cloud service, a different computer, external drive, USB)." >> ~/Documents/user_info.txt
