97d368e0c7730833e8f9a6ee75f093697ae043b8
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-12-03 21:35:25 -0700

    Update and rename git.sh to gitea.sh

--- gitea.sh
+++ gitea.sh
@@ -4,8 +4,10 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-# Install Docker
-sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+# Install necessary packages
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release zenity
+
+# Add Docker package repository
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
@@ -15,15 +17,29 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# Get user information
+USER_INFO=$(zenity --forms --title="User Information" \
+    --text="Enter user information" \
+    --add-entry="Username" \
+    --add-password="Password" \
+    --add-entry="Domain")
+
+# Parse user information
+USERNAME=$(echo $USER_INFO | cut -d'|' -f1)
+PASSWORD=$(echo $USER_INFO | cut -d'|' -f2)
+DOMAIN=$(echo $USER_INFO | cut -d'|' -f3)
+
 # Setup Gitea
 mkdir -p /var/lib/gitea
 docker run -d --name=gitea -p 10022:22 -p 10080:3000 -v /var/lib/gitea:/data gitea/gitea:latest
 GITEA_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' gitea)
 
-# Setup GitLab
-sudo docker run --detach --hostname gitlab.example.com --publish 443:443 --publish 80:80 --publish 22:22 --name gitlab --restart always --volume /srv/gitlab/config:/etc/gitlab --volume /srv/gitlab/logs:/var/log/gitlab --volume /srv/gitlab/data:/var/opt/gitlab gitlab/gitlab-ce:latest
-GITLAB_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' gitlab)
-
 # Save IP addresses and directions to a document
 echo "Gitea is hosted at: $GITEA_IP:10080" > ~/Documents/selfhosted_gits
-echo "GitLab is hosted at: $GITLAB_IP:80" >> ~/Documents/selfhosted_gits
+
+# Create a document with user information
+echo "Username: $USERNAME" > ~/Documents/user_info.txt
+echo "Password: $PASSWORD" >> ~/Documents/user_info.txt
+echo "Domain: $DOMAIN" >> ~/Documents/user_info.txt
+echo "" >> ~/Documents/user_info.txt
+echo "Please save this file in a different location (example: a cloud service, a different computer, external drive, USB)." >> ~/Documents/user_info.txt
